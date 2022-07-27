7b14f16e8adaaa70f7087dedf13b0f865f2625c3
Ravi Shrestha <rabishrestha@hotmail.com>
2022-07-27 07:51:09 +0545

    Add project files.

---
+++ ProjectReact/ClientApp/src/AppRoutes.js
@@ -0,0 +1,25 @@
+import { Counter } from "./components/Counter";
+import { FetchData } from "./components/FetchData";
+import { Home } from "./components/Home";
+import { Student } from "./components/Student";
+
+const AppRoutes = [
+  {
+    index: true,
+    element: <Home />
+  },
+  {
+    path: '/counter',
+    element: <Counter />
+  },
+  {
+    path: '/fetch-data',
+    element: <FetchData />
+    },
+    {
+        path: '/student',
+        element: <Student />
+    }
+];
+
+export default AppRoutes;
---
+++ ProjectReact/ClientApp/src/components/Student.js
@@ -0,0 +1,41 @@
+﻿import React, { Component } from 'react';
+
+export class Student extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstname: "Rabi"
+        }
+
+    };
+
+    handleFirstname = (event) => {
+        this.setState({ firstname: event.target.value });
+        console.log(event.target.value);
+    }
+
+    handleSubmit = (event) => {
+        let fn = this.state.firstname;
+        if (fn == "")
+            alert("firstname is required");
+        else {
+            console.log(fn);
+            alert(`My Firstname is ${this.state.firstname}`);
+        }
+        event.preventDefault();
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <h1>Student</h1>
+                    Firstname <input type="text" value={this.state.firstname} onChange={this.handleFirstname} />
+
+                    <button className="btn btn-primary" >Submit</button>
+                </div>
+            </form>
+        );
+
+    }
+}
